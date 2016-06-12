a105c4295f27a2ec74a84336df9e8bb9bab80b7c
paoldilo <paoldilo@yahoo.it>
2016-06-12 18:08:29 +0200

    ver 5

---
+++ part0.R
@@ -0,0 +1,49 @@
+
+#sample <- function(filename, prob){
+#     original <- file(paste(filename),"r")
+#     file <- readLines(original)
+#     # sampling with rbinom()
+#     set.seed(5150)
+#     reduced <- file[rbinom(n = length(file), size = 1, prob = prob) == 1]
+#     close(original)
+#     conn <- file(paste(filename, "_red", ".txt",sep=""), "w")
+#     writeLines(reduced, con = conn)
+#     close(conn)}
+#sample(filename = "./en_US.blogs.txt",prob = 0.10)
+#sample(filename = "./en_US.news.txt",prob = 0.10)
+#sample(filename = "./en_US.twitter.txt",prob = 0.10)
+#aa <- Corpus(DirSource("./"),readerControl = list(reader = readPlain,language = "la",load = TRUE))
+# countLines("./en_US/en_US.blogs.txt")
+# countLines("./en_US/en_US.news.txt")
+# countLines("./en_US/en_US.twitter.txt")
+set.seed(5150)
+percentage <- .03
+## sample blogs at text percentage
+original <- file("./en_US/en_US.blogs.txt","r")
+file_blog <- readLines(original)
+reduced <- file_blog[rbinom(n = length(file_blog), size = 1, prob = percentage) == 1]
+close(original)
+conn <- file("./en_US/en_US.blogs_reduced.txt", "w")
+writeLines(reduced, con = conn)
+close(conn)
+## sample news at text percentage
+original <- file("./en_US/en_US.news.txt","r")
+file_news <- readLines(original)
+reduced <- file_news[rbinom(n = length(file_news), size = 1, prob = percentage) == 1]
+close(original)
+conn <- file("./en_US/en_US.news_reduced.txt", "w")
+writeLines(reduced, con = conn)
+close(conn)
+## sample twitter at text percentage
+original <- file("./en_US/en_US.twitter.txt","r")
+file_twit <- readLines(original)
+reduced <- file_twit[rbinom(n = length(file_twit), size = 1, prob = percentage) == 1]
+close(original)
+conn <- file("./en_US/en_US.twitter_reduced.txt", "w")
+writeLines(reduced, con = conn)
+close(conn)
+#save memory resources
+rm(file_news)
+rm(file_blog)
+rm(file_twit)
+rm(reduced)
---
+++ part1.R
@@ -0,0 +1,40 @@
+
+
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("Rgraphviz")
+library(Rgraphviz)
+library(R.utils)
+library(tm)
+
+# create a new reduced corpus
+mainCorpus<-Corpus(DirSource("./en_US/",pattern = "reduced"))
+# clean the data
+mainCorpus<-tm_map(mainCorpus,removePunctuation)
+mainCorpus<-tm_map(mainCorpus, stripWhitespace)
+mainCorpus<-tm_map(mainCorpus, content_transformer(tolower))
+mainCorpus<-tm_map(mainCorpus,removeNumbers)
+
+TDM_stopwords <- TermDocumentMatrix(mainCorpus, control = list(stopwords = FALSE))
+limit <- 5000
+high_stop <-findFreqTerms(TDM_stopwords, limit, Inf)
+m_stop <- as.matrix(TDM_stopwords)
+v_stop <- sort(rowSums(m_stop), decreasing=TRUE)
+#plot(head(v_stop, 15) ,type = 'h',main = "Most frequent words including Stopwords")
+#plot(TDM_stopwords,terms=names(v_stop[1:20]), corThreshold = 0.8, weighting = F)
+
+TDM_nostopwords <- TermDocumentMatrix(mainCorpus, control = list(stopwords = TRUE))
+high_nostop <-findFreqTerms(TDM_nostopwords, limit, Inf)
+m_nostop <- as.matrix(TDM_nostopwords)
+v_nostop <- sort(rowSums(m_nostop), decreasing=TRUE)
+#plot(head(v_nostop, 15) ,type = 'h',main = "Most frequent words including Stopwords")
+#plot(TDM_nostopwords,terms=names(v_nostop[1:25]), corThreshold = 0.8, weighting = F)
+
+
+#Can I use bigrams instead of single tokens in a term-document matrix?
+#Yes. Package NLP provides functionality to compute n-grams which can be used to construct a corresponding tokenizer. E.g.:
+     
+BigramTokenizer <- function(x)
+          unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
+
+tdm <- NGramTokenizer(mainCorpus, control = list(tokenize = BigramTokenizer))
+inspect(removeSparseTerms(tdm[, 1:10], 0.7))
